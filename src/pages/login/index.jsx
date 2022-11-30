97c41e2afd52ba8a721c172bb1ff82c8d874d477
cebarrico <cbarrico@gmail.com>
2022-11-30 12:37:23 -0300

    login page

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,3 +1,27 @@
+import { FormLogin } from "./style";
+import { ButtonDefault } from "../../styles/button";
+import { InputDefault } from "../../styles/input";
+import "./style.css";
+import logo from "../../assets/img/Logo.png";
+
 export const LoginPage = () => {
-  return <h1>Login</h1>;
+  return (
+    <main>
+      <img src={logo} alt="kenzie hub" />
+      <FormLogin>
+        <h1>Login</h1>
+        <div>
+          <label name="">Email</label>
+          <InputDefault type="email" name="" id="" />
+        </div>
+        <div>
+          <label name="">Password</label>
+          <InputDefault type="password" />
+        </div>
+        <ButtonDefault>Entrar</ButtonDefault>
+        <span>Ainda não possui uma conta?</span>
+        <ButtonDefault>Cadastrar</ButtonDefault>
+      </FormLogin>
+    </main>
+  );
 };
---
+++ src/styles/button.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const ButtonDefault = styled.button`
+  width: 90%;
+  height: 38px;
+
+  background-color: var(--color-primary);
+  color: var(--color-grey-0);
+
+  border: 1px solid var(--color-primary);
+  border-radius: 4px;
+
+  cursor: pointer;
+`;
