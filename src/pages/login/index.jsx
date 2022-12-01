c954a95cfaf44d8406c2d75d6e26375fa96a24ce
cebarrico <cbarrico@gmail.com>
2022-12-01 10:53:12 -0300

    initial input components

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,13 +1,18 @@
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+import { useNavigate } from "react-router-dom";
+
+import { api } from "../../services/api";
+
+import { loginSchema } from "./LoginSchema";
 import { FormDefault } from "../../styles/form";
 import { ButtonDefault } from "../../styles/button";
 import { InputDefault } from "../../styles/input";
+import { Input } from "../../components/Inputs";
+
 import "./style.css";
 import logo from "../../assets/img/Logo.png";
-import { useForm } from "react-hook-form";
-import { useNavigate } from "react-router-dom";
-import { yupResolver } from "@hookform/resolvers/yup";
-import { loginSchema } from "./LoginSchema";
-import { api } from "../../services/api";
 
 export const LoginPage = () => {
   const navigate = useNavigate();
@@ -32,7 +37,8 @@ export const LoginPage = () => {
         console.log(err);
       }
     }
-    makeLogin();
+    console.log(data);
+    // makeLogin();
   }
 
   return (
@@ -49,6 +55,12 @@ export const LoginPage = () => {
           />
           {errors.email?.message && <p>{errors.email.message}</p>}
         </div>
+        {/* <Input
+          type="email"
+          label="Email"
+          placeholder="Coloque seu email"
+          register={register("email")}
+        /> */}
         <div>
           <label name="">Password</label>
           <InputDefault
